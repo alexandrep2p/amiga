7781267a46ed728593d3166ee5206795b392c420
Alexandre Abreu <alexandre_1605@outlook.com>
2018-01-22 21:11:55 -0200

    Mascaras e alteração diretório
    Adicionado algumas máscaras nos inputs, adicionado diretório para JS e CSS

--- index.php
+++ index.php
@@ -1,139 +1,74 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-<style>
-* {
-  box-sizing: border-box;
-}
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"><!--Fontes do form-->
+    <script type="text/javascript" src="assets/jquery.js"></script><!--Necessário para as mascaras dos inputs-->
+    <script type="text/javascript" src="assets/jquery.maskedinput.js"></script><!--Necessário para as mascaras dos inputs-->
+    <link rel="stylesheet" type="text/css" href="assets/style.css"><!--CSS do form, não é o bootstrap-->
+  </head>
 
-body {
-  background-color: #f1f1f1;
-}
+  <!--Método para as máscaras dos inputs--> 
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $("#cpf").mask("999.999.999-99");
+      $("#data").mask("99/99/9999");
+      $("#cep").mask("99.999-999");
+      $("#telefone").mask("(99)9999-9999");
+      $("#celular").mask("(99)99999-9999");
+      $("#email").mask("999.999.999-99");
+    });
+  </script>
 
-#regForm {
-  background-color: #ffffff;
-  margin: 100px auto;
-  font-family: Raleway;
-  padding: 40px;
-  width: 70%;
-  min-width: 300px;
-}
-
-h1 {
-  text-align: center;  
-}
-
-p {
-  text-align: center;
-}
-
-input {
-  padding: 10px;
-  width: 100%;
-  font-size: 17px;
-  font-family: Raleway;
-  border: 1px solid #aaaaaa;
-}
-
-/* Altera cor dos inputs que são requireds */
-input.invalid {
-  background-color: #ffdddd;
-}
-
-/* Esconde todos os steps por padrão: */
-.tab {
-  display: none;
-}
-
-button {
-  background-color: #4CAF50;
-  color: #ffffff;
-  border: none;
-  padding: 10px 20px;
-  font-size: 17px;
-  font-family: Raleway;
-  cursor: pointer;
-}
-
-button:hover {
-  opacity: 0.8;
-}
-
-#prevBtn {
-  background-color: #bbbbbb;
-}
-
-/* Bolinhas que mostram o progresso do formulario */
-.step {
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbbbbb;
-  border: none;  
-  border-radius: 50%;
-  display: inline-block;
-  opacity: 0.5;
-}
-
-.step.active {
-  opacity: 1;
-}
-
-/* Muda a cor da bolinha do step que esta preenchido e válido */
-.step.finish {
-  background-color: #4CAF50;
-}
-</style>
-<body>
-
-<form id="regForm" action="/action_page.php">
-  <h1>16º Encontro - "De volta ao berço"</h1>
-  <p>Associação dos Militares Inativos de Guaratinguetá e Adjacências</p>
-  <p>R: Mórmons, no 51 – Pedregulho – Guaratinguetá/ SP – CEP: 12.515-100- tel: (12) 3125-7798/ 3125-7843</p>
-  <p>www.amigafa.com – email: encontrao@amigafa.com</p>
-  <!-- Cada "tab" é um step do form: -->
-  <div class="tab">Dados Pessoais:
-    <p><input class="notNull" placeholder="CPF" oninput="this.className = ''" name="mil_cpf"></p>
-    <p><input placeholder="RG Civil" oninput="this.className = ''" name="mil_rgCivil"></p>
-    <p><input placeholder="RG Militar" oninput="this.className = ''" name="mil_rgMilitar"></p>
-    <p><input class="notNull" placeholder="Nome" oninput="this.className = ''" name="mil_nome"></p>
-    <p><input placeholder="Nome de Guerra" oninput="this.className = ''" name="mil_nomeGuerra"></p>
-    <p><input placeholder="Posto/Graduação" oninput="this.className = ''" name="mil_postoGrad"></p>
-    <p><input class="notNull" placeholder="Data de Formatura: DD/MM/AAAA" oninput="this.className = ''" name="mil_dataFormatura"></p>
-  </div>
-  <div class="tab">Endereço:
-    <p><input class="notNull" placeholder="CEP" oninput="this.className = ''" name="mil_cpf"></p>
-    <p><input class="notNull" placeholder="Endereço" oninput="this.className = ''" name="mil_endereco"></p>
-    <p><input placeholder="Numero" oninput="this.className = ''" name="mil_numeroEndereco"></p>
-    <p><input placeholder="Complemento" oninput="this.className = ''" name="mil_complementoEndereco"></p>
-    <p><input class="notNull" placeholder="Bairro" oninput="this.className = ''" name="mil_bairro"></p>
-    <p><input class="notNull" placeholder="Cidade" oninput="this.className = ''" name="mil_cidade"></p>
-    <p><input class="notNull" placeholder="Estado/UF" oninput="this.className = ''" name="mil_estado"></p>
-  </div>
-  <div class="tab">Contatos:
-    <p><input placeholder="Telefone: (XX) XXXX-XXXX" oninput="this.className = ''" name="mil_telefone"></p>
-    <p><input placeholder="Celular: (XX) XXXXX-XXXX" oninput="this.className = ''" name="mil_celular"></p>
-    <p><input class="notNull" placeholder="Email:" oninput="this.className = ''" name="mil_email"></p>
-  </div>
-  <div class="tab">Login Info:
-    <p><input placeholder="Username..." oninput="this.className = ''" name="uname"></p>
-    <p><input placeholder="Password..." oninput="this.className = ''" name="pword" type="password"></p>
-  </div>
-  <div style="overflow:auto;">
-    <div style="float:right;">
-      <button type="button" id="prevBtn" onclick="nextPrev(-1)">Voltar</button>
-      <button type="button" id="nextBtn" onclick="nextPrev(1)">Próximo</button>
-    </div>
-  </div>
-  <!-- Bolinhas que marcam o progresso, colocar na quantidade de steps que tem no form: -->
-  <div style="text-align:center;margin-top:40px;">
-    <span class="step"></span>
-    <span class="step"></span>
-    <span class="step"></span>
-    <span class="step"></span>
-  </div>
-</form>
+  <body>
+    <form id="regForm" action="/action_page.php">
+      <h1>16º Encontro - "De volta ao berço"</h1>
+      <p>Associação dos Militares Inativos de Guaratinguetá e Adjacências</p>
+      <p>R: Mórmons, no 51 – Pedregulho – Guaratinguetá/ SP – CEP: 12.515-100- tel: (12) 3125-7798/ 3125-7843</p>
+      <p>www.amigafa.com – email: encontrao@amigafa.com</p>
+      <!-- Cada "tab" é um step do form: -->
+      <div class="tab">Dados Pessoais:
+        <p><input id="cpf"class="notNull" placeholder="CPF" oninput="this.className = ''" name="mil_cpf"></p>
+        <p><input placeholder="RG Civil" oninput="this.className = ''" name="mil_rgCivil"></p>
+        <p><input placeholder="RG Militar" oninput="this.className = ''" name="mil_rgMilitar"></p>
+        <p><input class="notNull" placeholder="Nome" oninput="this.className = ''" name="mil_nome"></p>
+        <p><input placeholder="Nome de Guerra" oninput="this.className = ''" name="mil_nomeGuerra"></p>
+        <p><input placeholder="Posto/Graduação" oninput="this.className = ''" name="mil_postoGrad"></p>
+        <p><input id="data" class="notNull" placeholder="Data de Formatura: DD/MM/AAAA" oninput="this.className = ''" name="mil_dataFormatura"></p>
+      </div>
+      <div class="tab">Endereço:
+        <p><input id="cep" class="notNull" placeholder="CEP" oninput="this.className = ''" name="mil_cpf"></p>
+        <p><input class="notNull" placeholder="Endereço" oninput="this.className = ''" name="mil_endereco"></p>
+        <p><input placeholder="Numero" oninput="this.className = ''" name="mil_numeroEndereco"></p>
+        <p><input placeholder="Complemento" oninput="this.className = ''" name="mil_complementoEndereco"></p>
+        <p><input class="notNull" placeholder="Bairro" oninput="this.className = ''" name="mil_bairro"></p>
+        <p><input class="notNull" placeholder="Cidade" oninput="this.className = ''" name="mil_cidade"></p>
+        <p><input class="notNull" placeholder="Estado/UF" oninput="this.className = ''" name="mil_estado"></p>
+      </div>
+      <div class="tab">Contatos:
+        <p><input id="telefone" placeholder="Telefone: (XX) XXXX-XXXX" oninput="this.className = ''" name="mil_telefone"></p>
+        <p><input id="celular" placeholder="Celular: (XX) XXXXX-XXXX" oninput="this.className = ''" name="mil_celular"></p>
+        <p><input id="email" class="notNull" placeholder="Email:" oninput="this.className = ''" name="mil_email"></p>
+      </div>
+      <div class="tab">Login Info:
+        <p><input placeholder="Username..." oninput="this.className = ''" name="uname"></p>
+        <p><input placeholder="Password..." oninput="this.className = ''" name="pword" type="password"></p>
+      </div>
+      <div style="overflow:auto;">
+        <div style="float:right;">
+          <button type="button" id="prevBtn" onclick="nextPrev(-1)">Voltar</button>
+          <button type="button" id="nextBtn" onclick="nextPrev(1)">Próximo</button>
+        </div>
+      </div>
+      <!-- Bolinhas que marcam o progresso, colocar na quantidade de steps que tem no form: -->
+      <div style="text-align:center;margin-top:40px;">
+        <span class="step"></span>
+        <span class="step"></span>
+        <span class="step"></span>
+        <span class="step"></span>
+      </div>
+    </form>
+  </body>
 
 <script>
 var currentTab = 0; // A tab padrão é a tab 0 (0)
@@ -208,7 +143,5 @@ function fixStepIndicator(n) {
   //... and adds the "active" class on the current step:
   x[n].className += " active";
 }
-</script>
-
-</body>
+  </script>
 </html>
